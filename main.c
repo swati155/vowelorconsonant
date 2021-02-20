31e566e15628ba3fc36c74c54bab48400389ac4f
swati155 <72041157+swati155@users.noreply.github.com>
2021-02-20 21:55:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char c;
+    printf("enter alphabet:\n");
+    scanf("%c",&c);
+    if(c=='a')
+    {
+        printf("this is vowel",c);
+    }
+
+    if(c=='e')
+    {
+        printf("this is vowel",c);
+    }
+    if(c=='i')
+    {
+        printf("this is vowel",c);
+    }
+    if(c=='o')
+    {
+        printf("this is vowel",c);
+    }
+    if(c=='u')
+    {
+        printf("this is vowel",c);
+    }
+    else
+    {
+        printf("this is consonant");
+    }
+
+    return 0;
+}
